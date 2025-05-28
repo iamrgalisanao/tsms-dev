6a669c7d8a1c36341a5ac3d456c187248c7b614d
rommel galisanao <rommel.galisanao.22@gmail.com>
2025-05-28 15:44:34 +0800

    fix pos_termainal create table migration file

--- database/migrations/2025_04_05_050412_create_pos_terminals_table.php
+++ database/migrations/2025_04_05_050412_create_pos_terminals_table.php
@@ -8,20 +8,22 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('pos_terminals', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('tenant_id')->constrained();
-            $table->string('terminal_uid')->unique();
-            $table->enum('status', ['active', 'inactive', 'suspended'])->default('active');
-            $table->boolean('is_sandbox')->default(false);
-            $table->string('webhook_url')->nullable();
-            $table->integer('max_retries')->default(3);
-            $table->integer('retry_interval_sec')->default(300);
-            $table->boolean('retry_enabled')->default(true);
-            $table->string('jwt_token');
-            $table->timestamp('registered_at');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('pos_terminals')) {
+            Schema::create('pos_terminals', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('tenant_id')->constrained();
+                $table->string('terminal_uid')->unique();
+                $table->enum('status', ['active', 'inactive', 'suspended'])->default('active');
+                $table->boolean('is_sandbox')->default(false);
+                $table->string('webhook_url')->nullable();
+                $table->integer('max_retries')->default(3);
+                $table->integer('retry_interval_sec')->default(300);
+                $table->boolean('retry_enabled')->default(true);
+                $table->string('jwt_token');
+                $table->timestamp('registered_at');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
