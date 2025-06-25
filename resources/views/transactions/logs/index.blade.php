33f2dda73374a092abec3102eef939137960582f
rommel galisanao <rommel.galisanao.22@gmail.com>
2025-06-25 14:52:57 +0800

    fix getstatusbadge() undefined error

--- resources/views/transactions/logs/index.blade.php
+++ resources/views/transactions/logs/index.blade.php
@@ -44,8 +44,8 @@ use App\Helpers\BadgeHelper;
                     <td>{{ $log->transaction_id }}</td>
                     <td>{{ $log->terminal->identifier ?? 'N/A' }}</td>
                     <td class="text-end">₱{{ number_format($log->gross_sales, 2) }}</td>
-                    <td class="text-center">{!! BadgeHelper::getStatusBadge($log->validation_status) !!}</td>
-                    <td class="text-center">{!! BadgeHelper::getStatusBadge($log->job_status, 'job') !!}</td>
+                    <td class="text-center">{!! BadgeHelper::getValidationStatusBadge($log->validation_status) !!}</td>
+                    <td class="text-center">{!! BadgeHelper::getJobStatusBadge($log->job_status, 'job') !!}</td>
                     <td class="text-center">{{ $log->job_attempts }}</td>
                     <td class="text-center">{{ $log->transaction_count }}</td>
                     <td class="text-center">{{ formatDate($log->completed_at) }}</td>
