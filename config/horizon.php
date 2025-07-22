40575169ce13bf2966dba808cdc09f371743ff0d
rommel galisanao <rommel.galisanao.22@gmail.com>
2025-07-22 10:42:26 +0800

    add auth and is.admin to horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -48,7 +48,11 @@ return [
     | the chance to add your own middleware to this list or change any of
     | the existing middleware. Or, you can simply stick with this list.
     */
-    'middleware' => ['web'],
+    'middleware' => [
+        'web',
+        'is.admin',
+        'auth',
+        ],
 
     /*
     |--------------------------------------------------------------------------
