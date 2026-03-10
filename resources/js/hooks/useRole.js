de61aa1b03f280521bee02252bf9f2599d8d722b
Rommel Galisanao <rommel.galisanao.22@gmail.com>
2026-03-10 15:55:46 +0800

    commit

--- resources/js/hooks/useRole.js
+++ resources/js/hooks/useRole.js
@@ -2,6 +2,14 @@ import { useAuth } from '../Contexts/AuthContext';
 
 export const useRole = () => {
     const { user } = useAuth();
-    console.log('useRole debug:', user ? user.role : 'no user');
+    console.log('[useRole] Debug Info:', {
+        user,
+        role: user ? user.role : null,
+        email: user ? user.email : null,
+        name: user ? user.name : null,
+        rolesArray: user && user.roles ? user.roles : null,
+        isAdmin: user ? user.role === 'admin' : false,
+        isCommercial: user ? user.role === 'commercial' : false,
+    });
     return user && (user.role === 'admin' || user.role === 'commercial');
 };
